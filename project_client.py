de03f0aec64f5c470e1298d522a0dea5b01d37a5
DanteA <cristina_aquino@dlsu.edu.ph>
2019-03-30 22:28:30 +0800

    Add files via upload
    Initial Commit

---
+++ project_client.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+import random
+import os
+
+"""CLIENT CODE"""
+
+
+def receive_data(sock):
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            print(data.decode('utf-8'))
+        except:
+            pass
+
+
+def run_client(server_ip):
+    host = socket.gethostbyname(socket.gethostname())
+    port = random.randint(6000, 10000)
+    print('Client IP->' + str(host) + ' Port->' + str(port))
+    server = (str(server_ip), 5000)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    name = raw_input('Enter username: ')
+    if name == '':
+        name = 'Guest' + str(random.randint(1000, 9999))
+    print('Your name is: ' + '[' + name + ']')
+    s.sendto(name.encode('utf-8'), server)
+    threading.Thread(target=receive_data, args=(s,)).start()
+    while True:
+        data = raw_input()
+        if data == 'qqq':
+            break
+        elif data == '':
+            continue
+        data = '[' + name + ']' + '->' + data
+        s.sendto(data.encode('utf-8'), server)
+    s.close()
+    os._exit(1)
+
+
+if __name__ == '__main__':
+    run_client("10.100.201.46")
---
+++ project_server.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+import Queue  # renamed to queue in 3.7
+
+"""SERVER CODE"""
+# TODO: stop-and-wait, pipelining, flow and congestion control
+# TODO: quiz game code
+
+
+def recv_data(sock, recv_packets):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        recv_packets.put((data, addr))
+
+
+def run_server():
+    host = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    print('Server hosting on IP->' + str(host))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    clients = set()
+    recv_packets = Queue.Queue()
+
+    print('Server Running...')
+
+    threading.Thread(target=recv_data, args=(s, recv_packets)).start()
+    while True:
+        while not recv_packets.empty():
+            data, addr = recv_packets.get()
+            if addr not in clients:
+                clients.add(addr)
+                continue
+            clients.add(addr)
+            data = data.encode('utf-8')
+            if data.endswith('qqq'):
+                clients.remove(addr)
+                continue
+            print(str(addr) + data)
+            for c in clients:
+                if c != addr:
+                    s.sendto(data.encode('utf-8'), c)
+    s.close()
+
+
+if __name__ == '__main__':
+    run_server()
